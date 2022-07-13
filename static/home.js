3e93c9ea2d03176d7bd1b61eeacadaef9e111caa
Dae-une <ko301201@naver.com>
2022-07-13 13:52:43 +0900

    이걸로 사용

--- static/home.js
+++ static/home.js
@@ -26,9 +26,6 @@ $descending.addEventListener("click", (e) => {
                                                 <button class="spreadBtn${i}" onclick="spread(${i})" id="rightBtn${i}"><i class="fa-solid fa-arrow-down"></i></button>
                                             </div>
                                             <ul class="commentList ${i}" id="commentList${i}">
-                                                <li>김철수 : 짱짱 !</li>
-                                                <li>박종철 : 맛있어요 !</li>
-                                                <li>신윤섭 : 부드러워요 !</li>
                                             </ul>
                                          </div>`;
           $(".card-group").prepend(html_temp);
@@ -102,59 +99,8 @@ $.ajax({
 });
 
 function save_comment(index) {
-
-    let comment = $(`.commentBox${index}`).val();
-    let nick = $('.myNickName').text()
-    if (comment === '') {
-        alert('댓글을 입력해 주세요!')
-        return
-    } else {
-        $.ajax({
-            type: "POST",
-            url: "/cocktail",
-            data: {comment_give: comment, index_give: index, nick_give: nick},
-            success: function (response) {
-                alert(response["msg"])
-                window.location.reload()
-            }
-        });
-    }
-
-}
-
-function moveDetailPage() {
-    window.location.href = 'https://www.google.com/'
-}
-
-function spread(index) {
-    let commentArea = `<input type="text" class="commentBox${index}" placeholder="댓글을 입력하세요 !">
-                       <button class="inputBtn" onclick="save_comment(${index})">입력</button>`
-    let moreCommentBtn = `<li><button class='moreCommentBtn' onclick='moveDetailPage()'><i class="fa-solid fa-ellipsis"></i></button></li>`
-    $(`.${index}`).empty()
-
-    $(`.${index}`).append(commentArea)
-    $.ajax({
-        type: "get",
-        url: "/cocktail",
-        success: function (result) {
-            let reviewList = [];
-            let rows = result['comment'];
-            console.log(rows);
-            rows.forEach((item) => {
-                let text = `<li>${item['nick']}: ${item['comment']}</li>`
-                if (item['index'] == index) {
-                    reviewList.push(text);
-                }
-            })
-            let cuReview = reviewList.slice(-5).reverse();
-            $(`.${index}`).append(cuReview);
-            $(`.${index}`).append(moreCommentBtn)
-            // console.log(rows[0]['index']);
-            // console.log(rows['comment']);
-            // rows.forEach()
-        },
-
   let comment = $(`.commentBox${index}`).val();
+  let nick = $(".myNickName").text();
   if (comment === "") {
     alert("댓글을 입력해 주세요!");
     return;
@@ -162,45 +108,43 @@ function spread(index) {
     $.ajax({
       type: "POST",
       url: "/cocktail",
-      data: { comment_give: comment, index_give: index },
+      data: { comment_give: comment, index_give: index, nick_give: nick },
       success: function (response) {
         alert(response["msg"]);
         window.location.reload();
       },
-
     });
   }
 }
-
-
-    const id = document.querySelector(`#rightBtn${index}`)
-    if (id.className == `spreadBtn${index}`) {
-        $(`.${index}`).show();
-        id.setAttribute('class', `closeBtn${index}`)
-    } else if (id.className == `closeBtn${index}`) {
-        $(`.${index}`).hide();
-        id.setAttribute('class', `spreadBtn${index}`)
-    }
+function moveDetailPage() {
+  window.location.href = "https://www.google.com/";
+}
 
 function spread(index) {
+  let commentArea = `<input type="text" class="commentBox${index}" placeholder="댓글을 입력하세요 !">
+                     <button class="inputBtn" onclick="save_comment(${index})">입력</button>`;
+  let moreCommentBtn = `<li><button class='moreCommentBtn' onclick='moveDetailPage()'><i class="fa-solid fa-ellipsis"></i></button></li>`;
   $(`.${index}`).empty();
+
+  $(`.${index}`).append(commentArea);
   $.ajax({
     type: "get",
     url: "/cocktail",
     success: function (result) {
+      let reviewList = [];
       let rows = result["comment"];
+      console.log(rows);
       rows.forEach((item) => {
-        let text = `<li>${item["comment"]}</li>`;
+        let text = `<li>${item["nick"]}: ${item["comment"]}</li>`;
         if (item["index"] == index) {
-          $(`.${index}`).append(text);
+          reviewList.push(text);
         }
       });
-      // console.log(rows[0]['index']);
-      // console.log(rows['comment']);
-      // rows.forEach()
+      let cuReview = reviewList.slice(-5).reverse();
+      $(`.${index}`).append(cuReview);
+      $(`.${index}`).append(moreCommentBtn);
     },
   });
-
 
   const id = document.querySelector(`#rightBtn${index}`);
   if (id.className == `spreadBtn${index}`) {
@@ -214,9 +158,9 @@ function spread(index) {
 
 function filter() {
   let search = document.getElementById("search").value.toLowerCase();
-  // console.log(search);
+  console.log(search);
   let list = document.getElementsByClassName("card");
-  // console.log(list);
+  console.log(list);
   for (let i = 0; i < list.length; i++) {
     let searchname = list[i];
     if (searchname.innerText.toLowerCase().includes(search)) {
